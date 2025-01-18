1502d4011c3f2e133387f2fb0aeddbf4a29ef00a
root <root@docker05.jaredbloomer.local>
2025-01-18 00:40:56 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,64 @@
+FROM openjdk:25-slim
+
+# Set defaults for environment variables
+ENV MINECRAFT_PORT 25565
+ENV RCON_PORT 27015
+ENV JAVA_MEMORY 1G
+ENV RCON_ENABLED false
+ENV WHITELIST_ENABLED true
+ENV ALLOW_NETHER false
+ENV GAME_MODE creative
+ENV ENABLE_QUERY false
+ENV PLAYER_IDLE_TIMEOUT 0
+ENV DIFFICULTY easy
+ENV SPAWN_MONSTERS false
+ENV SPAWN_ANIMALS false
+ENV SPAWN_NPCS false
+
+ENV LEVEL_TYPE default
+ENV PVP false
+ENV BROADCAST_CONSOLE_TO_OPS true
+ENV SPAWN_PROTECTION 16
+ENV MAX_TICK_TIME 60000
+ENV FORCE_GAMEMODE true
+#ENV GENERATOR_SETTINGS
+ENV OP_PERMISSION_LEVEL 4
+ENV SNOOPER_ENABLED true
+ENV HARDCORE false
+ENV ENABLE_COMMAND_BLOCK false
+ENV MAX_PLAYERS 10000
+ENV NETWORK_COMPRESSION_THRESHOLD 256
+#ENV RESOURCE_PACK_SHA1
+ENV MAX_WORLD_SIZE 29999984
+#ENV SERVER_IP
+ENV ALLOW_FLIGHT true
+ENV LEVEL_NAME world
+ENV VIEW_DISTANCE 10
+ENV GENERATE_STRUCTURES true
+ENV ONLINE_MODE true
+ENV MAX_BUILD_HEIGHT 256
+#ENV LEVEL_SEED
+ENV PREVENT_PROXY_CONNECTION false
+
+WORKDIR /minecraft
+
+RUN apt-get update && apt-get install -y libfreetype6 curl wget
+
+# Install rcon
+RUN curl -L -o rcon-cli.tar.gz https://github.com/itzg/rcon-cli/releases/download/1.6.9/rcon-cli_1.6.9_linux_amd64.tar.gz && \
+  tar -xzf rcon-cli.tar.gz && \
+  rm rcon-cli.tar.gz && \
+  mv rcon-cli /usr/local/bin
+
+# Setup the server
+RUN curl -L -o fabric-installer.jar https://maven.fabricmc.net/net/fabricmc/fabric-installer/1.0.1/fabric-installer-1.0.1.jar && \
+  java -jar fabric-installer.jar server -downloadMinecraft -mcversion 1.21.1
+
+# Copy the signed eula
+COPY ./eula.txt eula.txt
+
+# Add the entrypoint
+COPY ./entrypoint.sh /minecraft/entrypoint.sh
+COPY ./server.properties /server.properties
+
+ENTRYPOINT [ "/minecraft/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,129 @@
+#!/bin/sh -e
+
+  if [ "${WORLD_BACKUP}" != "" ]; then
+    echo "Loading World From Backup..."
+
+    if [ ! "$(ls -A /minecraft/world)" ]; then 
+      echo "Installing default world ${WORLD_BACKUP}"
+    
+      if [ ! -d "/minecraft/world" ]; then
+        mkdir /minecraft/world
+      fi
+   
+      cd /tmp && \
+        wget -O world.tar.gz ${WORLD_BACKUP} && \
+        tar -C /minecraft/world -xzf world.tar.gz && \
+        rm world.tar.gz
+    fi
+  fi
+
+  if [ "${PLUGINS_BACKUP}" != "" ]; then 
+    if [ ! "$(ls -A /minecraft/plugins)" ]; then 
+      echo "Installing default plugins ${PLUGINS_BACKUP}"
+    
+      if [ ! -d "/minecraft/plugins" ]; then
+        mkdir /minecraft/plugins
+      fi
+
+      cd /tmp && \
+        wget -O plugins.tar.gz ${PLUGINS_BACKUP} && \
+        tar -C /minecraft/plugins -xzf plugins.tar.gz && \
+        rm plugins.tar.gz
+    fi
+  fi
+
+  if [ "${MODS_BACKUP}" != "" ]; then 
+    if [ ! "$(ls -A /minecraft/mods)" ]; then 
+      echo "Installing default mods ${MODS_BACKUP}"
+    
+      if [ ! -d "/minecraft/mods" ]; then
+        mkdir /minecraft/mods
+      fi
+
+      cd /tmp && \
+        wget -O mods.tar.gz ${MODS_BACKUP} && \
+        tar -C /minecraft/mods -xzf mods.tar.gz && \
+        rm mods.tar.gz
+    fi
+  fi
+
+  if [ ! -d "/minecraft/config" ]; then
+    mkdir /minecraft/config
+  fi
+
+  if [ ! -f "/minecraft/config/banned-ips.json" ]; then
+    echo "[]" >> /minecraft/config/banned-ips.json
+  fi
+
+  if [ ! -f "/minecraft/config/banned-players.json" ]; then
+    echo "[]" >> /minecraft/config/banned-players.json
+  fi
+
+  if [ ! -f "/minecraft/config/usercache.json" ]; then
+    echo "[]" >> /minecraft/config/usercache.json
+  fi
+
+  if [ ! -f "/minecraft/config/whitelist.json" ]; then
+    echo "[]" >> /minecraft/config/whitelist.json
+  fi
+
+  if [ ! -f "/minecraft/config/bukkit.yml" ]; then
+    cat <<EOF > /minecraft/config/bukkit.yml
+      # This is the main configuration file for Bukkit.
+      # As you can see, there's actually not that much to configure without any plugins.
+      # For a reference for any variable inside this file, check out the Bukkit Wiki at
+      # https://www.spigotmc.org/go/bukkit-yml
+      #
+      # If you need help on this file, feel free to join us on irc or leave a message
+      # on the forums asking for advice.
+      #
+      # IRC: #spigot @ irc.spi.gt
+      #    (If this means nothing to you, just go to https://www.spigotmc.org/go/irc )
+      # Forums: https://www.spigotmc.org/
+      # Bug tracker: https://www.spigotmc.org/go/bugs
+    
+    
+      settings:
+        allow-end: true
+        warn-on-overload: true
+        permissions-file: permissions.yml
+        update-folder: update
+        plugin-profiling: false
+        connection-throttle: 4000
+        query-plugins: true
+        deprecated-verbose: default
+        shutdown-message: Server closed
+        minimum-api: none
+      spawn-limits:
+        monsters: 70
+        animals: 10
+        water-animals: 5
+        water-ambient: 20
+        ambient: 15
+      chunk-gc:
+        period-in-ticks: 600
+      ticks-per:
+        animal-spawns: 400
+        monster-spawns: 1
+        water-spawns: 1
+        water-ambient-spawns: 1
+        ambient-spawns: 1
+        autosave: 6000
+      aliases: now-in-commands.yml
+EOF
+  fi
+
+  ln -s /minecraft/config/banned-ips.json /minecraft/banned-ips.json
+  ln -s /minecraft/config/banned-players.json /minecraft/banned-players.json
+  ln -s /minecraft/config/usercache.json /minecraft/usercache.json
+  ln -s /minecraft/config/whitelist.json /minecraft/whitelist.json
+  ln -s /minecraft/config/ops.json /minecraft/ops.json
+  ln -s /minecraft/config/bukkit.yml /minecraft/bukkit.yml
+
+  # Configure the properties
+  # Echo the file as it has embedded environment variables
+  eval "echo \"$(cat /server.properties)\"" > /minecraft/server.properties
+
+  cd /minecraft
+  java -Xmx${JAVA_MEMORY} -Xms${JAVA_MEMORY} -Dfml.queryResult=confirm -jar fabric-server-launch.jar nogui
+
